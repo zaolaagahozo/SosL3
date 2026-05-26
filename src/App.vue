fbfe0a44ae423081acdf273d84e2c2c2d0df493d
zaolaagahozo <zaolaagahozo8@gmail.com>
2026-05-26 09:46:08 +0200

    my first commit on first project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <title>Sos l3</title>
+  <div class="bg-gray-100 min-h-screen p-6">
+    <div class="flex flex-col items-center gap-4">
+      <div class="flex items-center gap-4">
+        <img
+          src="/src/assets/logo.png"
+          alt="SOS Logo"
+          class="w-20 h-20 object-contain"
+        />
+        <h1 class="text-4xl font-extrabold text-blue-600 underline">
+          SOS SCHOOL BLOG
+        </h1>
+      </div>
+      <p class="text-center text-gray-700 mt-3 italic">
+        Learn Today, Lead Tomorrow
+      </p>
+    </div>
+      <h2 class="text-2xl font-bold text-blue-500 border-b-2 border-blue-300 pb-2">
+        About SOS</h2>
+      
+     <p class="mt-3 text-gray-700">
+        SOS School is a place where students learn, grow, and prepare for their future. 
+        The school helps students improve their knowledge, discipline, and creativity.
+      </p>
+
+      <h2 class="text-2xl font-bold text-blue-500 border-b-2 border-blue-300 pb-2">
+        History</h2>
+        <p class="mt-3 text-gray-700">
+        SOS School was created to provide quality education and support to students.
+        SOS Children’s Villages Rwanda is an independent member association affiliated to the SOS Federation. As a leader in childcare, SOS Children’s Villages Rwanda leverages existing community resources to strengthen quality child care and protection through a holistic continuum of care tailored to orphans, vulnerable children and their families. SOS Children’s Villages Rwanda actively advocates for the promotion of childcare and child protection.
+Since 2004, The First Lady of Rwanda, Her Excellency Mrs. Jeannette Kagame is the Patron of SOS Children’s Village Rwanda.
+      </p>
+
+  </div>
+</template>
