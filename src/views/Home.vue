e21376268d8f928c3e1f6f8f777752fc82bc7eae
zaolaagahozo <zaolaagahozo8@gmail.com>
2026-05-28 20:16:12 +0200

    my first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <script setup></script>
 <template>
 <!--this is home page-->
-<section class="relative h-screen bg-cover bg-center flex-items justify-center text-white"
-style="background-image:url('/src/assets/p.pp.png');"
+<section class="relative h-screen bg-cover bg-center flex items-center justify-center text-white"
+  style="background-image:url('/src/assets/tembera .png');"
 >
-
-
+  <h1 class="text-4xl font-bold text-center">welcome to kigali Rwanda</h1>
 </section>
 
 
