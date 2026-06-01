a42260297c305416ab6539fcb33405da89e15510
zaolaagahozo <zaolaagahozo8@gmail.com>
2026-06-01 16:03:21 +0200

    add the rest of the work

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: // important for vercel / static hosting
   plugins: [
     vue(),
     vueDevTools(),
